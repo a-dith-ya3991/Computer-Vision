2e3379425a367389c622c33076a9eaf2ec8dbca6
a-dith-ya3991 <104985561+a-dith-ya3991@users.noreply.github.com>
2022-10-31 21:07:18 +0530

    Add files via upload

---
+++ inverse scaling.py
@@ -0,0 +1,19 @@
+import cv2 
+import numpy as np
+import matplotlib.pyplot as ply
+def scale(img,x,y):
+    s=np.array([[x,0],
+               [0,y]])
+    s_inv=np.linalg.inv(s)
+    h,w=img.shape[0],img.shape[1]
+    img2=np.zeros((int(h*x),int(w*y),3),dtype="uint8")
+    
+    for new_i in range(int(h*x)):
+        for new_j in range(int(w*y)):
+            p=s_inv.dot(np.array([new_i,new_j]))
+            i,j=np.floor(p[0]),np.floor(p[1])
+            img2[new_i,new_j]=img[int(i),int(j)]
+    return img2
+img=cv2.imread(r"C:\Users\ravij\Downloads\lotus.jpg")
+img2=scale(img,2,2)
+ply.imshow(img2[:,:,::-1])
