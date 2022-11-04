6f8ed14966e44de6c569610b07011aaa852950df
a-dith-ya3991 <104985561+a-dith-ya3991@users.noreply.github.com>
2022-11-04 17:53:33 +0530

    Update 2.2.reflection operation.py

--- 2.2.reflection operation.py
+++ 2.2.reflection operation.py
@@ -14,9 +14,9 @@ import numpy as np
 import matplotlib.pyplot as pyp
 def rot(img,m):
     
-    sh,sw=img.shape[0],img.shape[1]#source iumage height , width
+    sh,sw=img.shape[0],img.shape[1]#source image height , width
     corners=np.array([[0,0],[0,sw-1],[sh-1,0],[sh-1,sw-1]])
-    newcord=m.dot(corners.T)#here we take transpose because to cnahge dimensions
+    newcord=m.dot(corners.T)#here we take transpose because to change dimensions
     rmin,cmin=np.floor(min(newcord[0])),np.floor(min(newcord[1]))
     rmax,cmax=np.ceil(max(newcord[0])),np.ceil(max(newcord[1]))
     dh=np.int32(rmax-rmin+1)#destination image height,width
@@ -29,7 +29,7 @@ def rot(img,m):
             img2[new_i][new_j]=img[i][j]
     return img2
 img=cv2.imread(r"PATH")# Replace "PATH" with your image Path
-m=np.array([[1,0],# Replace the matrix accotding to required form
+m=np.array([[1,0],# Replace the matrix according to required form
            [0,-1]])
 img2=rot(img,m)
 fig,ax=pyp.subplots(1,2)
